e1971f13923a3653cbd77152bedf683eddeb1766
giftobabyworld <giftobabyworld@gmail.com>
2024-06-07 21:49:00 +0000

    fvfv

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAkl_kY5rykqoFb3i8hRzBDofTC4oaaLOk",
+    authDomain: "giftobabyworld-7812d.firebaseapp.com",
+    projectId: "giftobabyworld-7812d",
+    storageBucket: "giftobabyworld-7812d.appspot.com",
+    messagingSenderId: "372226449792",
+    appId: "1:372226449792:web:c126b386474035d239a165",
+    measurementId: "G-56V04MXN6G"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
