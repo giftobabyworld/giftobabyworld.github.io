05abdef0b21617f9abcaef48ec8f965171fff605
Hamza Durrani <hamzashakeelhaiderdurrani77@gmail.com>
2024-07-11 19:32:39 +0500

    thankyou and footer done

--- script.js
+++ script.js
@@ -112,6 +112,24 @@ function scrollToTop() {
   });
 }
 
+// -----------------Confetti thankyoiu---------------------------
+const start = () => {
+  setTimeout(function () {
+    confetti.start();
+  }, 200); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+};
+
+//  Stop
+
+const stop = () => {
+  setTimeout(function () {
+    confetti.stop();
+  }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+};
+
+start();
+stop();
+
 // -----------------Products slider---------------------------
 
 const swiperEl = document.querySelector("swiper-container");
@@ -189,4 +207,3 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 // ----------------------------------------------backend---------------------------------
-
